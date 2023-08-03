f90b7cf5635be2368669edac32af6ca459c909d9
Munira <munirashovokhobova@gmail.com>
2023-08-03 23:53:43 +0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { ROUTERS_PATH } from "@/constans";
+
+import Home from "@/pages/Home.vue";
+
+export const routers = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: ROUTERS_PATH.HOME, name: ROUTERS_PATH.HOME, component: Home },
+        {
+            path: ROUTERS_PATH.COCKTAIL,
+            name: ROUTERS_PATH.COCKTAIL,
+            component: () =>
+                import ('@/pages/Cocktail.vue')
+        },
+        {
+            path: ROUTERS_PATH.COCKTAIL_RANDOM,
+            name: ROUTERS_PATH.COCKTAIL_RANDOM,
+            component: () =>
+                import ('@/pages/CocktailRandom.vue')
+        },
+
+    ]
+})
